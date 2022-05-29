dab227bdd125e9e385d54361391a3f6270496927
djeevanantham <105917599+djeevanantham@users.noreply.github.com>
2022-05-29 22:22:08 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,168 @@
+from imutils.object_detection import non_max_suppression
+import numpy as np
+import time
+import cv2
+import pytesseract
+
+import os
+import os.path
+
+if(os.path.exists("./1.txt")):
+    os.remove("./1.txt")
+    
+with open('1.txt', 'w') as fp:
+    pass
+cam = cv2.VideoCapture(0)
+cam.set(3, 320)
+cam.set(4, 240)
+cv2.namedWindow("test")
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+
+img_counter = 1
+
+def text_detector(image):
+	orig = image
+	(H, W) = image.shape[:2]
+
+	(newW, newH) = (320, 320)
+	rW = W / float(newW) 
+    
+	rH = H / float(newH)
+
+	image = cv2.resize(image, (newW, newH))
+	(H, W) = image.shape[:2]
+
+	layerNames = [
+		"feature_fusion/Conv_7/Sigmoid",
+		"feature_fusion/concat_3"]
+
+
+	blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),
+		(123.68, 116.78, 103.94), swapRB=True, crop=False)
+
+	net.setInput(blob)
+	(scores, geometry) = net.forward(layerNames)
+
+	(numRows, numCols) = scores.shape[2:4]
+	rects = []
+	confidences = []
+
+	for y in range(0, numRows):
+
+		scoresData = scores[0, 0, y]
+		xData0 = geometry[0, 0, y]
+		xData1 = geometry[0, 1, y]
+		xData2 = geometry[0, 2, y]
+		xData3 = geometry[0, 3, y]
+		anglesData = geometry[0, 4, y]
+
+		# loop over the number of columns
+		for x in range(0, numCols):
+			# if our score does not have sufficient probability, ignore it
+			if scoresData[x] < 0.5:
+				continue
+
+			# compute the offset factor as our resulting feature maps will
+			# be 4x smaller than the input image
+			(offsetX, offsetY) = (x * 4.0, y * 4.0)
+
+			# extract the rotation angle for the prediction and then
+			# compute the sin and cosine
+			angle = anglesData[x]
+			cos = np.cos(angle)
+			sin = np.sin(angle)
+
+			# use the geometry volume to derive the width and height of
+			# the bounding box
+			h = xData0[x] + xData2[x]
+			w = xData1[x] + xData3[x]
+
+			# compute both the starting and ending (x, y)-coordinates for
+			# the text prediction bounding box
+			endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+			endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
+			startX = int(endX - w)
+			startY = int(endY - h)
+
+			# add the bounding box coordinates and probability score to
+			# our respective lists
+			rects.append((startX, startY, endX, endY))
+			confidences.append(scoresData[x])
+
+	boxes = non_max_suppression(np.array(rects), probs=confidences)
+
+	for (startX, startY, endX, endY) in boxes:
+
+		startX = int(startX * rW)
+		startY = int(startY * rH)
+		endX = int(endX * rW)
+		endY = int(endY * rH)
+		boundary = 2
+
+		text = orig[startY-boundary:endY+boundary, startX - boundary:endX + boundary]
+		text = cv2.cvtColor(text.astype(np.uint8), cv2.COLOR_BGR2GRAY)
+		textRecongized = pytesseract.image_to_string(text, lang='eng', config='--psm 7 --oem 1 -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyzABCDEFGIJKLMNOPQRSTUVWXYZ1234567890')
+		#print(textRecongized)
+		with open("1.txt",'a') as f: f.write(str(textRecongized))
+		original_file = "1.txt"
+		temp_file = "temp.txt"
+
+		string_to_delete = ['\x0c', '|']
+		with open(original_file, "r") as input:
+		    with open(temp_file, "w") as output:
+		        for line in input:
+		            for word in string_to_delete:
+		                line = line.replace(word, "")
+		            output.write(line)
+
+# replace file with original name
+		#os.replace('temp.txt', '1.txt')
+		with open('temp.txt', 'r') as file_r, open('1.txt', 'w') as file_w:
+		    thresh = 3
+		    for line in file_r:
+		        if len(line) > thresh:
+		            file_w.write(line)
+
+		cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 3)
+		orig = cv2.putText(orig, textRecongized, (endX,endY+5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2, cv2.LINE_AA) 
+	return orig
+
+while True:
+  while True:
+      ret, frame = cam.read()
+      if not ret:
+          print("failed to grab frame")
+          break
+      cv2.imshow("test", frame)
+
+      k = cv2.waitKey(1)
+      if k%256 == 32:
+        # SPACE pressed
+          img_name = "imagein.png"
+          cv2.imwrite(img_name, frame)
+          print("Predicting...")
+          break
+      
+  cam.release()
+
+  cv2.destroyAllWindows()
+  net = cv2.dnn.readNet("frozen_east_text_detection.pb")
+
+
+
+  image0 = cv2.imread('imagein.png')
+
+
+  imageO = cv2.resize(image0, (640,320), interpolation = cv2.INTER_AREA)
+  orig = cv2.resize(image0, (640,320), interpolation = cv2.INTER_AREA)
+  textDetected = text_detector(imageO)
+#cv2.imshow("Orig Image",orig)
+  cv2.imshow("Text Detection", textDetected)
+  file1 = open("1.txt","r")
+  print(file1.readlines())
+  print()
+  file1.close()
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
+  cam = cv2.VideoCapture(0)
+cv2.destroyAllWindows()
